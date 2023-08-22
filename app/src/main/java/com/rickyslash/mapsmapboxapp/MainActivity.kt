d36d87bd7b0ce9d92609ed694fbc19dd800c80ac
ricky-kiva <ricky.kiva@gmail.com>
2023-08-23 04:52:19 +0700

    feat(presentation-main): add Mapbox on MainActivity

--- app/src/main/java/com/rickyslash/mapsmapboxapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/mapsmapboxapp/MainActivity.kt
@@ -2,10 +2,65 @@ package com.rickyslash.mapsmapboxapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.Style
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mapView: MapView
+    private lateinit var btnNavigation: Button
+    private lateinit var mapboxMap: MapboxMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapView)
+        btnNavigation = findViewById(R.id.btnNavigation)
+
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync { mapboxMap ->
+            this.mapboxMap = mapboxMap
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
